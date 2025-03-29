e6d75a9c7230be5ddd95c0205695d002005d12f0
abel kasheto <lufulokasheto@gmail.com>
2025-03-29 11:01:18 +0300

    am adding text

--- v1/config/db.php
+++ v1/config/db.php
@@ -7,5 +7,7 @@ return [
     'charset' => 'utf8mb4'
 ];
 
+// https://github.com/lubiyu/cs421-assignment.git
+
 // http://ec2-16-171-159-76.eu-north-1.compute.amazonaws.com:9000/students
 // http://ec2-16-171-159-76.eu-north-1.compute.amazonaws.com:9000/subjects
